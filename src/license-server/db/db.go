92f7707cf24e3e3c97b96dd792bd9a034921fe81
Ahmad Zaidi <azaidi4@wisc.edu>
2019-08-07 12:27:31 -0500

    Initialize table on startup

--- src/license-server/db/db.go
+++ src/license-server/db/db.go
@@ -4,25 +4,51 @@ import (
 	"database/sql"
 	"fmt"
 	"license-server/db/dbutils"
+
+	_ "github.com/lib/pq"
 )
 
-var dbConn *sql.DB
+var DBConn *sql.DB
 var err error
 
 func startConnection() {
 	connStr := dbutils.BuildDBString()
-	dbConn, err = sql.Open("postgres", connStr)
+	DBConn, err = sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Println("ERROR: ", err)
 		return
 	}
-	if err = dbConn.Ping(); err != nil {
+	if err = DBConn.Ping(); err != nil {
 		fmt.Println("ERROR: ", err)
 		return
 	}
 	fmt.Println("Connected to Database", connStr)
 }
 
+func initTable() {
+	init, err := DBConn.Prepare(
+		`CREATE TABLE IF NOT EXISTS dc_licenses(
+      id SERIAL PRIMARY KEY,
+      license_expiry DATE NOT NULL,
+      license_user_limit INT NOT NULL,
+      license_key BIGINT NOT NULL,
+      license_org VARCHAR NOT NULL
+    );
+    `)
+	defer init.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if _, err := init.Query(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Created table dc_licenses")
+}
+
 func init() {
 	startConnection()
+	initTable()
 }
